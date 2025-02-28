1ba1f4dbd123b52b52bf568e4d5cacc1f203e258
creativecoder2 <creativecoder93@gmail.com>
2025-03-01 01:52:55 +0500

    Fix GitHub Actions pipeline setup

--- landingscreen/landing.spec.js
+++ landingscreen/landing.spec.js
@@ -6,5 +6,5 @@ test('should be titled', async ({ page, context }) => {
   await context.route('**.jpg', route => route.abort());
   await page.goto('https://example.com/');
 
-  await expect(page).toHaveTitle('Example');
+  await expect(page).toHaveTitle('Example Domain');
 });
